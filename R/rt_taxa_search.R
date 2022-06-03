7f1259ab776f0aa8a28cf8b58e179f7c25e97187
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2022-06-03 14:52:33 +0200

    Add version argument (partial fix #21)

--- R/rt_taxa_search.R
+++ R/rt_taxa_search.R
@@ -65,6 +65,10 @@
 #' @param vernacular  {`character(1+)`}\cr{}
 #'                    Name of a vernacular group (see the list using
 #'                    [`rt_vernacular_groups()`])
+#' @param version {`NULL or character(1)`}\cr{}
+#'                A string indicating which version of TAXREF should be queried.
+#'                Should be of the form `"2.0"` up to `"15.0"`. You can check
+#'                the available TAXREF versions with `rt_taxref_versions()`.
 #' @param page {`integer(1)`; default = `1`}\cr{}
 #'             The page number returned
 #' @param size {`integer(1)`; default = `5000`}\cr{}
@@ -76,20 +80,25 @@
 #' @export
 rt_taxa_search = function(sciname = NULL, fr_name = NULL, en_name = NULL,
                           rank = NULL, territories = NULL, domain = NULL,
-                          habitats = NULL, vernacular = NULL, page = 1,
-                          size = 5000) {
+                          habitats = NULL, vernacular = NULL, version = NULL,
+                          page = 1, size = 5000) {
 
-  api_query = rt_GET("taxa/search",
-                     query = list(scientificNames = sciname,
-                                  frenchVernacularNames = fr_name,
-                                  englishVernacularNames = en_name,
-                                  taxonomicRanks = rank,
-                                  territories = territories,
-                                  domain = domain,
-                                  habitats = habitats,
-                                  vernacularGroups = vernacular,
-                                  page = page,
-                                  size = size))
+  api_query = rt_GET(
+    "taxa/search",
+    query = list(
+      scientificNames        = sciname,
+      frenchVernacularNames  = fr_name,
+      englishVernacularNames = en_name,
+      taxonomicRanks         = rank,
+      territories            = territories,
+      domain                 = domain,
+      habitats               = habitats,
+      vernacularGroups       = vernacular,
+      version                = version,
+      page                   = page,
+      size                   = size
+    )
+  )
 
   parse_taxa(api_query)
 }
