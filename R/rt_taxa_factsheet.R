a32e1323ae38671c22f7f6cd0c94338df9ecab38
Matthias Grenié <matthias.grenie@ens-lyon.fr>
2022-06-04 12:51:32 +0200

    Add rt_taxa_factsheet()

---
+++ R/rt_taxa_factsheet.R
@@ -0,0 +1,17 @@
+#' Get factsheet for taxa
+#'
+#' For some taxa, a factsheet has been written. This text, often in French,
+#' describes the general ecology and some diagnosis of the submitted taxon.
+#'
+#' Beware, many taxa have no corresponding factsheet.
+#'
+#' @inheritParams rt_taxa_id
+#'
+#' @examples
+#' rt_taxa_factsheet(4540)
+#' @export
+rt_taxa_factsheet = function(id) {
+  api_query = rt_GET("taxa/", id, "/factsheet")
+
+  parse_taxa(api_query)
+}
