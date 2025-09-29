b97899e679324f1610759b90b193ac5e028bad9a
Matthias Grenié <matthias.grenie@univ-grenoble-alpes.fr>
2025-09-29 11:57:50 +0200

    Add Startup message about MNHN hack

---
+++ R/zzz.R
@@ -0,0 +1,11 @@
+# Script that contains miscellaneous functions
+.onAttach = function(...) {
+
+  packageStartupMessage(
+  '🛑Important September 2025 Update🛑
+   The Natural History Museum of Paris has been hacked, TaxREF went offline, including its API
+   So, for now rtaxref won’t work
+      To see how to circumvent the issue refer to our README page: https://rekyt.github.io/rtaxref/'
+  )
+
+}
