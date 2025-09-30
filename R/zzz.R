40e7cf676ec7e24fe8ccca65d70a50154be6737f
Matthias Grenié <matthias.grenie@univ-grenoble-alpes.fr>
2025-09-30 10:13:52 +0200

    Update GitHub actions (#41)
    * Update GitHub actions
    
    * Fix message to ASCII
    
    * Fix message to ASCII 2

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,9 @@
 .onAttach = function(...) {
 
   packageStartupMessage(
-  '🛑Important September 2025 Update🛑
+  '/!\ Important September 2025 Update /!\
    The Natural History Museum of Paris has been hacked, TaxREF went offline, including its API
-   So, for now rtaxref won’t work
+   So, for now rtaxref won\'t work
       To see how to circumvent the issue refer to our README page: https://rekyt.github.io/rtaxref/'
   )
 
