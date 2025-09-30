82e192f35104a8c221d2a20de79aa27157446ce4
Matthias Grenié <matthias.grenie@univ-grenoble-alpes.fr>
2025-09-30 09:47:07 +0200

    Fix message to ASCII

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 .onAttach = function(...) {
 
   packageStartupMessage(
-  '🛑Important September 2025 Update🛑
+  '/!\ Important September 2025 Update /!\
    The Natural History Museum of Paris has been hacked, TaxREF went offline, including its API
    So, for now rtaxref won’t work
       To see how to circumvent the issue refer to our README page: https://rekyt.github.io/rtaxref/'
